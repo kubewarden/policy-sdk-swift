8d9327e4aa170d572db5317e94ee0d5eef500683
Flavio Castelli <fcastelli@suse.com>
2021-04-28 10:47:25 +0200

    Add ability to mutate requests

--- Package.swift
+++ Package.swift
@@ -12,15 +12,14 @@ let package = Package(
             targets: ["kubewardenSdk"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "GenericJSON", url: "https://github.com/zoul/generic-json-swift.git", from: "2.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "kubewardenSdk",
-            dependencies: []),
+            dependencies: ["GenericJSON"]),
         .testTarget(
             name: "kubewardenSdkTests",
             dependencies: ["kubewardenSdk"]),
--- Sources/kubewardenSdk/validation.swift
+++ Sources/kubewardenSdk/validation.swift
@@ -1,4 +1,5 @@
 import Foundation
+import GenericJSON
 
 /// Describes the ValidationRequest object given to the policy
 /// inside of the `validate` guest function.
@@ -21,6 +22,7 @@ public struct ValidationResponse : Codable {
   let accepted: Bool
   let message: String?
   let code: Int?
+  let mutatedObject: JSON?
 }
 
 /// This function accepts the admission request as valid
@@ -30,13 +32,34 @@ public func acceptRequest() -> String {
   let response = ValidationResponse(
     accepted: true,
     message: nil,
-    code: nil)
+    code: nil,
+    mutatedObject: nil)
 
   let encoder = JSONEncoder()
   let data = try! encoder.encode(response)
 
   return String(data: data, encoding: .utf8)!
 }
+
+/// This function accepts the admission request as valid
+///
+/// - Parameter mutatedObject: an instance of `GenericJSON.JSON` that holds the new object
+/// - Returns: A `ValidationResponse` object serialized to JSON
+/// - SeeAlso: [GenericJSON](https://github.com/zoul/generic-json-swift): this library provides a way to serialize and deserialize generic JSON structures
+public func mutateRequest(mutatedObject: JSON) -> String {
+  let response = ValidationResponse(
+    accepted: true,
+    message: nil,
+    code: nil,
+    mutatedObject: mutatedObject)
+
+  let encoder = JSONEncoder()
+  encoder.keyEncodingStrategy = .convertToSnakeCase
+  let data = try! encoder.encode(response)
+
+  return String(data: data, encoding: .utf8)!
+}
+
 
 /// This function rejects the admission request as invalid
 ///
@@ -47,7 +70,8 @@ public func rejectRequest(message: String?, code: Int?) -> String {
   let response = ValidationResponse(
     accepted: false,
     message: message,
-    code: code)
+    code: code,
+    mutatedObject: nil)
 
   let encoder = JSONEncoder()
   let data = try! encoder.encode(response)
